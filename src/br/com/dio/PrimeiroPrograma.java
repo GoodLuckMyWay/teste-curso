b2ec2635b10ca8584cf59ab985ee964ab8105c8e
GoodLuckMyWay <italolemosdasilva@hotmail.com>
2023-03-29 16:50:31 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //System.out.println("Hello caralho");
+        // crl+shift+f10 compilar o programa
+        // crl + all L
+        // pesquisar na internet atalho do intellij
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
+
